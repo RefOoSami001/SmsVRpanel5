90e35ea53477242da893edf68aeb9b85e54b84dc
RefOoSami001 <159632003+RefOoSami001@users.noreply.github.com>
2024-10-06 17:23:48 +0300

    Update app.py

--- app.py
+++ app.py
@@ -1,6 +1,8 @@
 import sqlite3
 import requests
 import json
+from datetime import datetime
+import base64
 from flask import Flask, request, render_template, redirect, url_for, session, flash
 from datetime import datetime
 import re
@@ -195,16 +197,27 @@ def logout():
 def verification_code_finder():
     if not session.get('logged_in'):
         return redirect(url_for('login'))
+
     if 'user' in session:
         if request.method == 'POST':
             numbers = request.form['numbers'].split()
+            selected_api = request.form.get('api')
 
             total_success = 0
             total_fail = 0
             codes = {}
 
             for number in numbers:
-                code = get_panel_code(number)
+                if selected_api == '1':
+                    code = get_panel_code_api1(number)
+                elif selected_api == '2':
+                    code = get_panel_code_api2(number)
+                elif selected_api == '3':
+                    code = get_panel_code_api3(number)
+                else:
+                    flash('Please select an API.', 'danger')
+                    return render_template('verification.html')
+
                 status = 'Failed'
                 if code:
                     total_success += 1
@@ -227,9 +240,7 @@ def verification_code_finder():
         flash('Please log in first', 'danger')
         return redirect(url_for('login'))
 
-
-
-def get_panel_code(number):
+def get_panel_code_api1(number):
     cookies = {
         'PHPSESSID': 'j2amq0mh5848v110nk4imef651',
     }
@@ -244,12 +255,11 @@ def get_panel_code(number):
         'X-Requested-With': 'XMLHttpRequest',
     }
 
-
+    current_date = datetime.now().strftime('%Y-%m-%d')
     response = requests.get(
-        f'http://zodiacpanel.com/agent/res/data_smscdr.php?fdate1=2024-10-06%2000:00:00&fdate2=2024-10-06%2023:59:59&frange=&fclient=&fnum={number}&fcli=&fgdate=&fgmonth=&fgrange=&fgclient=&fgnumber=&fgcli=&fg=0&sEcho=1&iColumns=9&sColumns=%2C%2C%2C%2C%2C%2C%2C%2C&iDisplayStart=0&iDisplayLength=25&mDataProp_0=0&sSearch_0=&bRegex_0=false&bSearchable_0=true&bSortable_0=true&mDataProp_1=1&sSearch_1=&bRegex_1=false&bSearchable_1=true&bSortable_1=true&mDataProp_2=2&sSearch_2=&bRegex_2=false&bSearchable_2=true&bSortable_2=true&mDataProp_3=3&sSearch_3=&bRegex_3=false&bSearchable_3=true&bSortable_3=true&mDataProp_4=4&sSearch_4=&bRegex_4=false&bSearchable_4=true&bSortable_4=true&mDataProp_5=5&sSearch_5=&bRegex_5=false&bSearchable_5=true&bSortable_5=true&mDataProp_6=6&sSearch_6=&bRegex_6=false&bSearchable_6=true&bSortable_6=true&mDataProp_7=7&sSearch_7=&bRegex_7=false&bSearchable_7=true&bSortable_7=true&mDataProp_8=8&sSearch_8=&bRegex_8=false&bSearchable_8=true&bSortable_8=false&sSearch=&bRegex=false&iSortCol_0=0&sSortDir_0=desc&iSortingCols=1&_=1728219621497',
+        f'http://zodiacpanel.com/agent/res/data_smscdr.php?fdate1=2024-10-06%2000:00:00&fdate2={current_date}%2023:59:59&frange=&fclient=&fnum={number}&fcli=&fgdate=&fgmonth=&fgrange=&fgclient=&fgnumber=&fgcli=&fg=0&sEcho=1&iColumns=9&sColumns=%2C%2C%2C%2C%2C%2C%2C%2C&iDisplayStart=0&iDisplayLength=25&mDataProp_0=0&sSearch_0=&bRegex_0=false&bSearchable_0=true&bSortable_0=true&mDataProp_1=1&sSearch_1=&bRegex_1=false&bSearchable_1=true&bSortable_1=true&mDataProp_2=2&sSearch_2=&bRegex_2=false&bSearchable_2=true&bSortable_2=true&mDataProp_3=3&sSearch_3=&bRegex_3=false&bSearchable_3=true&bSortable_3=true&mDataProp_4=4&sSearch_4=&bRegex_4=false&bSearchable_4=true&bSortable_4=true&mDataProp_5=5&sSearch_5=&bRegex_5=false&bSearchable_5=true&bSortable_5=true&mDataProp_6=6&sSearch_6=&bRegex_6=false&bSearchable_6=true&bSortable_6=true&mDataProp_7=7&sSearch_7=&bRegex_7=false&bSearchable_7=true&bSortable_7=true&mDataProp_8=8&sSearch_8=&bRegex_8=false&bSearchable_8=true&bSortable_8=false&sSearch=&bRegex=false&iSortCol_0=0&sSortDir_0=desc&iSortingCols=1&_=1728219621497',
         cookies=cookies,
         headers=headers,
-        verify=False,
     )
     try:
         data = json.loads(response.text)
@@ -265,6 +275,86 @@ def get_panel_code(number):
             return None
     except:
         return None
+
+def get_panel_code_api2(number):
+    cookies = {
+        'PHPSESSID': 'e0al2dv65qkg7vrop7520qme0v',
+    }
+
+    headers = {
+        'Accept': 'application/json, text/javascript, */*; q=0.01',
+        'Accept-Language': 'ar-EG,ar;q=0.9,en-US;q=0.8,en;q=0.7',
+        'Connection': 'keep-alive',
+        # 'Cookie': 'PHPSESSID=nvlvbb5ro1u8p6p3rr126sldc5',
+        'Referer': 'http://109.236.81.102/ints/agent/SMSCDRStats',
+        'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/129.0.0.0 Safari/537.36',
+        'X-Requested-With': 'XMLHttpRequest',
+    }
+
+    # Get the current date in the required format (YYYY-MM-DD HH:MM:SS)
+    current_date = datetime.now().strftime('%Y-%m-%d')
+
+    response = requests.get(f'http://109.236.81.102/ints/agent/res/data_smscdr.php?fdate1=2022-05-01%2000:00:00&fdate2={current_date}%2023:59:59&frange=&fclient=&fnum={number}&fcli=&fgdate=&fgmonth=&fgrange=&fgclient=&fgnumber=&fgcli=&fg=0&sEcho=1&iColumns=9&sColumns=%2C%2C%2C%2C%2C%2C%2C%2C&iDisplayStart=0&iDisplayLength=25&mDataProp_0=0&sSearch_0=&bRegex_0=false&bSearchable_0=true&bSortable_0=true&mDataProp_1=1&sSearch_1=&bRegex_1=false&bSearchable_1=true&bSortable_1=true&mDataProp_2=2&sSearch_2=&bRegex_2=false&bSearchable_2=true&bSortable_2=true&mDataProp_3=3&sSearch_3=&bRegex_3=false&bSearchable_3=true&bSortable_3=true&mDataProp_4=4&sSearch_4=&bRegex_4=false&bSearchable_4=true&bSortable_4=true&mDataProp_5=5&sSearch_5=&bRegex_5=false&bSearchable_5=true&bSortable_5=true&mDataProp_6=6&sSearch_6=&bRegex_6=false&bSearchable_6=true&bSortable_6=true&mDataProp_7=7&sSearch_7=&bRegex_7=false&bSearchable_7=true&bSortable_7=true&mDataProp_8=8&sSearch_8=&bRegex_8=false&bSearchable_8=true&bSortable_8=false&sSearch=&bRegex=false&iSortCol_0=0&sSortDir_0=desc&iSortingCols=1&_=1728052116864',
+                        cookies=cookies, headers=headers)
+
+    try:
+        data = json.loads(response.text)
+
+        # Extract the message
+        message_text = data['aaData'][0][5]
+
+        # Search for the verification code
+        verification_code = re.search(r'\d+', message_text)
+            
+        if verification_code:
+            return verification_code.group()
+        else:
+            return None
+    except:
+        return None
+
+def get_panel_code_api3(number):
+    # Your credentials
+    username = "moh"
+    password = "550"
+
+    # Base64 encode the username and password
+    auth_hash = base64.b64encode(f"{username}:{password}".encode('utf-8')).decode('utf-8')
+
+    # Authorization header
+    headers = {
+        "Authorization": f"Basic {auth_hash}",
+        "x-current-page": "",
+        "x-page-count": "",
+        "x-per-page": "",
+        "x-total-count": ""
+    }
+
+    try:
+        # Send GET request to the API
+        response = requests.get("http://jorstel.com/rest/sms", headers=headers)
+
+        # Check if the request was successful
+        if response.status_code == 200:
+            data = response.json()  # Assuming the API returns a JSON response
+            found = False
+            for message in data:
+                if message.get('destination_addr') == str(number):
+                    found = True
+                    short_message = message.get('short_message')
+                    
+                    # Extract only the digits from the message using regex
+                    verification_code = re.findall(r'\d+', short_message)
+                    return verification_code[0]
+                    break
+            
+            if not found:
+                return None
+        else:
+            return None
+    except:
+        return None  
+
 if __name__ == '__main__':
     init_db()
     app.run(debug=False)
